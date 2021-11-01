7a208d607e9bc3375c535e9d2ab95724a4ef6e8a
Renan Roseno <rosenorenan@gmail.com>
2021-11-01 09:51:28 -0300

    Update package.json adding animations modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,19 @@ import { BannerComponent } from './acesso/banner/banner.component';
 import { LoginComponent } from './acesso/login/login.component';
 import { CadastroComponent } from './acesso/cadastro/cadastro.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
   declarations: [
     AppComponent,
     AcessoComponent,
     BannerComponent,
     LoginComponent,
-    CadastroComponent
+    CadastroComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
