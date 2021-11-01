239e4cc4cfcbacdcc7c0ee6542ff54df5e350104
Renan Roseno <rosenorenan@gmail.com>
2021-11-01 11:56:50 -0300

    Adding animation and pictures logic

--- src/app/acesso/banner/banner.component.ts
+++ src/app/acesso/banner/banner.component.ts
@@ -1,5 +1,6 @@
-import { state, style, trigger } from '@angular/animations';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+import { Picture } from 'src/app/model/picture.model';
 
 @Component({
   selector: 'app-banner',
@@ -12,17 +13,55 @@ import { Component, OnInit } from '@angular/core';
       })),
       state('visible', style({
         opacity: 1
-      }))
+      })),
+      transition('hidden <=> visible', animate('2s ease-in')), //view documentation //view documentation
     ])
   ]
 })
 export class BannerComponent implements OnInit {
 
   public initialState: string = 'hidden';
-  
+  // public images: Picture[] = [
+  //   { state: 'visible', url: '../../../assets/banner-acesso/img_1.png' },
+  //   { state: 'hidden', url: '../../../assets/banner-acesso/img_2.png' },
+  //   { state: 'hidden', url: '../../../assets/banner-acesso/img_3.png' },
+  //   { state: 'hidden', url: '../../../assets/banner-acesso/img_4.png' },
+  //   { state: 'hidden', url: '../../../assets/banner-acesso/img_5.png' }
+  // ]
+  public images: Picture[] = [
+    { state: 'visible', url: '../../../assets/banner-acesso/imteste1.png' },
+    { state: 'hidden', url: '../../../assets/banner-acesso/imteste2.png' },
+    { state: 'hidden', url: '../../../assets/banner-acesso/imteste3.png' },
+    { state: 'hidden', url: '../../../assets/banner-acesso/imteste4.png' },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
+    this.executeFunctionTime();
   }
 
+  public changeState(): void {
+
+    let aux: number = 0;
+    
+    for (let i: number = 0; i < this.images.length; i++) {
+      if (this.images[i].state == 'visible') {
+        this.images[i].state = 'hidden';
+        //this.images[i+1].state = 'visible';
+        aux = i === this.images.length - 1 ? 0 : i + 1;
+        console.log(this.images)
+        break;
+      }
+    }
+
+    this.images[aux].state = 'visible';
+    //setTimeout(() => { this.changeState() }, 2000)
+  this.executeFunctionTime();
+    //this.initialState = this.initialState == 'hidden' ? 'visible' : 'hidden';
+  }
+
+  private executeFunctionTime(): void {
+    setTimeout(() => { this.changeState() }, 3000)
+  }
 }
---
+++ src/app/model/picture.model.ts
@@ -0,0 +1,6 @@
+export class Picture {
+    constructor(
+        public state: string,
+        public url: string
+    ) { }
+}
