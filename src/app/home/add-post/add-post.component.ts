d90becd6c2e896b1e8cccd4ec2099fa916170d14
Renan Roseno <rosenorenan@gmail.com>
2021-11-09 11:16:42 -0300

    Adding progress dinamic

--- src/app/home/add-post/add-post.component.ts
+++ src/app/home/add-post/add-post.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { DataService } from 'src/app/service/data.service';
 import * as firebase from 'firebase';
-
+import { ProgressService } from 'src/app/service/progress.service';
+import { Observable, Subject } from 'rxjs';
+import { interval } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 @Component({
   selector: 'app-add-post',
   templateUrl: './add-post.component.html',
@@ -13,13 +16,18 @@ export class AddPostComponent implements OnInit {
   public form: FormGroup = new FormGroup({
     title: new FormControl(null),
   });
-
-  constructor(private dataService: DataService) {}
+  private image: any;
+  public progress: string = 'pendente';
+  public percertProgress: number = 0;
+  
+  constructor(
+    private dataService: DataService,
+    private progressService: ProgressService
+  ) {}
 
   ngOnInit(): void {
     firebase.auth().onAuthStateChanged((user) => {
       this.email = user?.email!;
-      console.log(user);
     });
   }
 
@@ -27,6 +35,28 @@ export class AddPostComponent implements OnInit {
     this.dataService.post({
       email: this.email,
       title: this.form.value.title,
+      image: this.image[0],
     });
+
+    const follower = interval(1500)
+    let cont = new Subject();
+
+    cont.next(true);
+
+    follower
+    .pipe(takeUntil(cont))
+    .subscribe(() => {
+      console.log(this.progressService.state);
+      console.log(this.progressService.status);
+      this.progress = 'andamento'
+      if(this.progressService.status === 'concluido'){
+        this.progress = 'concluido';
+        cont.next(false);
+      }
+    })
+  }
+
+  public prepareImageUpload(event: Event): void {
+    this.image = (<HTMLInputElement>event.target)!.files;
   }
 }
--- src/app/service/data.service.ts
+++ src/app/service/data.service.ts
@@ -1,16 +1,38 @@
 import { Injectable } from '@angular/core';
-import * as firebase from 'firebase'; 
-
+import * as firebase from 'firebase';
+import { ProgressService } from './progress.service';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataService {
-
-  constructor() { }
+  constructor(private progressService: ProgressService) {}
 
   public post(post: any): void {
-    console.log(post)
-    firebase.database().ref(`posts/${btoa(post.email)}`)
-    .push({title: post.title})
+    console.log(post);
+    let imageName = Date.now();
+
+    firebase
+      .storage()
+      .ref()
+      .child(`images/${imageName}`)
+      .put(post.image)
+      .on(
+        firebase.storage.TaskEvent.STATE_CHANGED,
+        (snapshot: any) => {
+          this.progressService.status = 'andamento';
+          this.progressService.state = snapshot;
+        },
+        (error) => {
+          this.progressService.status = 'erro';
+        },
+        () => {
+          this.progressService.status = 'concluido';
+        }
+      );
+
+    firebase
+      .database()
+      .ref(`posts/${btoa(post.email)}`)
+      .push({ title: post.title });
   }
 }
